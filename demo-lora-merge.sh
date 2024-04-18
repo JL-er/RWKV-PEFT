83fa68b2fb4774bb46fd29e190f879f7d84b09ca
JL-er <997529190@qq.com>
2024-04-18 15:42:05 +0800

    demo

--- demo-lora-merge.sh
+++ demo-lora-merge.sh
@@ -1,9 +1,16 @@
-QUANT='fp4'
+
+base_model='/home/rwkv/JL/model/rwkv-x060-7b-world-v2.1-36%trained-20240413-ctx4k.pth'
+lora_init='/home/rwkv/JL/out_model/nf4/init_lora.pth'
+lora_checkpoint='/home/rwkv/JL/out_model/nf4/rwkv-0.pth'
+output='/home/rwkv/JL/model/nf4-world.pth'
+QUANT='nf4' #follow train
 TYPE='lora'
 Lora_alpha=128
-python merge.py --base_model /home/rwkv/JL/model/rwkv-x060-7b-world-v2.1-36%trained-20240413-ctx4k.pth \
---lora_checkpoint /home/rwkv/JL/out_model/fp4/rwkv-0.pth \
---output /home/rwkv/JL/model/fp4-world.pth \
+
+python merge.py --base_model $base_model \
+--lora_init $lora_init \
+--lora_checkpoint $lora_checkpoint \
+--output $output \
 --quant $QUANT \
 --type $TYPE \
 --lora_alpha $Lora_alpha
--- demo-pissa-merge.sh
+++ demo-pissa-merge.sh
@@ -1,9 +1,15 @@
 
-QUANT='nf4'
 
-python merge.py --base_model /home/rwkv/JL/model/rwkv-x060-7b-world-v2.1-36%trained-20240413-ctx4k.pth \
---lora_init /home/rwkv/JL/out_model/nf4/init_lora.pth \
---lora_checkpoint /home/rwkv/JL/out_model/nf4/rwkv-0.pth \
---output /home/rwkv/JL/model/nf4-world.pth \
+base_model='/home/rwkv/JL/model/rwkv-x060-7b-world-v2.1-36%trained-20240413-ctx4k.pth'
+lora_init='/home/rwkv/JL/out_model/nf4/init_lora.pth'
+lora_checkpoint='/home/rwkv/JL/out_model/nf4/rwkv-0.pth'
+output='/home/rwkv/JL/model/nf4-world.pth'
+QUANT='nf4' #follow train
+TYPE='pissa'
+
+python merge.py --base_model $base_model \
+--lora_init $lora_init \
+--lora_checkpoint $lora_checkpoint \
+--output $output \
 --quant $QUANT \
---type pissa 
+--type $TYPE 
