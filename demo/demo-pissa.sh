2edb66b2b3f67e760b6666983a842c21b3b95f5a
J.L <139205286+JL-er@users.noreply.github.com>
2024-07-04 15:08:03 +0800

    Update demo-pissa.sh

--- demo/demo-pissa.sh
+++ demo/demo-pissa.sh
@@ -23,9 +23,12 @@ python train.py --load_model $load_model \
 --pre_ffn 0 --head_qk 0 --lr_init 5e-5 --lr_final 5e-5 --warmup_steps 0 --beta1 0.9 --beta2 0.99 --adam_eps 1e-8 \
 --accelerator gpu --devices 1 --precision bf16 --strategy deepspeed_stage_1 --grad_cp 1 \
 --my_testing "x060" \
---lora_load rwkv-0 --lora --lora_r $lora_r --lora_alpha 128 --lora_dropout 0.01 --lora_parts=att,ffn,time,ln \
+--lora --lora_r $lora_r --lora_alpha 128 --lora_dropout 0.01 --lora_parts=att,ffn,time,ln \
 --PISSA --svd_niter $svd_niter \
 --dataload pad --quant $QUANT
+
+###continue train
+--pissa_load '/home/rwkv/JL/out_model/nf4/rwkv-0.pth' --pissa_init '/home/rwkv/JL/out_model/nf4/init_pissa.pth'
 
 ###remove load_model
 # python train.py --proj_dir $proj_dir --data_file $data_file \
