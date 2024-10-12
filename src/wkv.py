70f7feb5a031e2eff4b3403a7c904fb7626143b1
Zhiyuan Li <uniartisan2017@gmail.com>
2024-10-12 22:52:54 +1100

    Combining duplicate cuda wkv6 patterns

--- src/wkv.py
+++ src/wkv.py
@@ -28,10 +28,13 @@ else:
     HEAD_SIZE = int(os.environ["RWKV_HEAD_SIZE_A"])
 
     if 'x060' in os.environ["RWKV_MY_TESTING"]:
-        if os.environ["RWKV_TRAIN_TYPE"] == 'infctx':
-            wkv6state_cuda = load(name="wkv6infctx", sources=["cuda/wkv6infctx_op.cpp", f"cuda/wkv6infctx_cuda.cu"],
-                            verbose=True, extra_cuda_cflags=["-res-usage", "--use_fast_math", "-O3", "-Xptxas -O3", "--extra-device-vectorization", f"-D_N_={HEAD_SIZE}", f"-D_T_={int(os.environ['RWKV_CTXLEN'])}"])
-                
+        if (os.environ["RWKV_TRAIN_TYPE"] == 'infctx') or (os.environ["RWKV_TRAIN_TYPE"] == 'states'):
+            if os.environ["RWKV_TRAIN_TYPE"] == 'infctx':
+                wkv6state_cuda = load(name="wkv6infctx", sources=["cuda/wkv6infctx_op.cpp", f"cuda/wkv6infctx_cuda.cu"],
+                                verbose=True, extra_cuda_cflags=["-res-usage", "--use_fast_math", "-O3", "-Xptxas -O3", "--extra-device-vectorization", f"-D_N_={HEAD_SIZE}", f"-D_T_={int(os.environ['RWKV_CTXLEN'])}"])
+            else:
+                wkv6state_cuda = load(name="wkv6state", sources=["cuda/wkv6state_op.cpp", f"cuda/wkv6state_cuda.cu"],
+                            verbose=True, extra_cuda_cflags=["-res-usage", "--use_fast_math", "-O3", "-Xptxas -O3", "--extra-device-vectorization", f"-D_N_={HEAD_SIZE}", f"-D_T_={int(os.environ['RWKV_CTXLEN'])}"])    
             class WKV_6STATE(torch.autograd.Function):
                 @staticmethod
                 def forward(ctx, B, T, C, H, r, k, v, w, u, s):
@@ -82,60 +85,6 @@ else:
             def RUN_CUDA_RWKV6_STATE(B, T, C, H, r, k, v, w, u, s):
                 x = WKV_6STATE.apply(B, T, C, H, r, k, v, w, u, s)
                 return x, s
-        elif os.environ["RWKV_TRAIN_TYPE"] == 'states':
-            wkv6state_cuda = load(name="wkv6state", sources=["cuda/wkv6state_op.cpp", f"cuda/wkv6state_cuda.cu"],
-                            verbose=True, extra_cuda_cflags=["-res-usage", "--use_fast_math", "-O3", "-Xptxas -O3", "--extra-device-vectorization", f"-D_N_={HEAD_SIZE}", f"-D_T_={int(os.environ['RWKV_CTXLEN'])}"])
-                
-            class WKV_6STATE(torch.autograd.Function):
-                @staticmethod
-                def forward(ctx, B, T, C, H, r, k, v, w, u, s):
-                    with torch.no_grad():
-                        assert r.dtype == torch.bfloat16
-                        assert k.dtype == torch.bfloat16
-                        assert v.dtype == torch.bfloat16
-                        assert w.dtype == torch.bfloat16
-                        assert u.dtype == torch.bfloat16
-                        assert s.dtype == torch.bfloat16
-                        assert HEAD_SIZE == C // H
-                        ctx.B = B
-                        ctx.T = T
-                        ctx.C = C
-                        ctx.H = H
-                        assert r.is_contiguous()
-                        assert k.is_contiguous()
-                        assert v.is_contiguous()
-                        assert w.is_contiguous()
-                        assert u.is_contiguous()
-                        assert s.is_contiguous()
-                        ctx.save_for_backward(r, k, v, w, u, s)
-                        y = torch.empty((B, T, C), device=r.device, dtype=torch.bfloat16, memory_format=torch.contiguous_format)#.uniform_(-100, 100)
-                        wkv6state_cuda.forward(B, T, C, H, r, k, v, w, u, s, y)
-                        return y
-
-                @staticmethod
-                def backward(ctx, gy):
-                    with torch.no_grad():
-                        assert gy.dtype == torch.bfloat16
-                        B = ctx.B
-                        T = ctx.T
-                        C = ctx.C
-                        H = ctx.H
-                        assert gy.is_contiguous()
-                        r, k, v, w, u, s = ctx.saved_tensors
-                        gr = torch.empty((B, T, C), device=gy.device, requires_grad=False, dtype=torch.bfloat16, memory_format=torch.contiguous_format)#.uniform_(-100, 100)
-                        gk = torch.empty((B, T, C), device=gy.device, requires_grad=False, dtype=torch.bfloat16, memory_format=torch.contiguous_format)#.uniform_(-100, 100)
-                        gv = torch.empty((B, T, C), device=gy.device, requires_grad=False, dtype=torch.bfloat16, memory_format=torch.contiguous_format)#.uniform_(-100, 100)
-                        gw = torch.empty((B, T, C), device=gy.device, requires_grad=False, dtype=torch.bfloat16, memory_format=torch.contiguous_format)#.uniform_(-100, 100)
-                        gu = torch.empty((B, C), device=gy.device, requires_grad=False, dtype=torch.bfloat16, memory_format=torch.contiguous_format)#.uniform_(-100, 100)
-                        gs = torch.empty((B, H, C//H, C//H), device=gy.device, requires_grad=False, dtype=torch.bfloat16, memory_format=torch.contiguous_format)#.uniform_(-100, 100)
-                        wkv6state_cuda.backward(B, T, C, H, r, k, v, w, u, s, gy, gr, gk, gv, gw, gu, gs)
-                        gu = torch.sum(gu, 0).view(H, C//H)
-                        gs = torch.sum(gs, 0).view(H, C//H, C//H)
-                        return (None, None, None, None, gr, gk, gv, gw, gu, gs)
-
-            def RUN_CUDA_RWKV6_STATE(B, T, C, H, r, k, v, w, u, s):
-                return WKV_6STATE.apply(B, T, C, H, r, k, v, w, u, s), None
-
         else:
             wkv6_cuda = load(name="wkv6", sources=["cuda/wkv6_op.cpp", f"cuda/wkv6_cuda.cu"],
                             verbose=True, extra_cuda_cflags=["-res-usage", "--use_fast_math", "-O3", "-Xptxas -O3", "--extra-device-vectorization", f"-D_N_={HEAD_SIZE}", f"-D_T_={int(os.environ['RWKV_CTXLEN'])}"])
