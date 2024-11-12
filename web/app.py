d39f69c94e954dc28070c50faa5c55c669ad2396
Ryan Luo <143677303+RyanLuohaha@users.noreply.github.com>
2024-11-12 18:42:38 +0800

    Gradio version is launched

--- web/app.py
+++ web/app.py
@@ -1,10 +1,30 @@
-import streamlit as st
-from common.utils import get_project_root
-from PIL import Image
+import gradio as gr
+from training import TrainingInterface
+from data import DataInterface
+from merge import MergeInterface
+from utils import get_project_root
 import os
-from components.sidebar import Sidebar
 
-st.set_page_config(layout="wide")
+# 添加 logo
+logo_path = os.path.join(get_project_root('app.py') + '/assets', 'logo.png')
 
-st.logo(Image.open(os.path.join(get_project_root() + '/web', 'assets/peft-logo.png')))
-Sidebar().show()
+def create_interface():
+    with gr.Blocks() as demo:
+        gr.Markdown(f"# RWKV-PEFT WEB")
+        
+        with gr.Tabs():
+            with gr.Tab("Train"):
+                training_interface = TrainingInterface()
+                training_interface.interface
+            with gr.Tab("Merge"):
+                merge_interface = MergeInterface()
+                merge_interface.interface
+            with gr.Tab("Data"):
+                data_interface = DataInterface()
+                data_interface.interface
+    
+    return demo
+
+if __name__ == "__main__":
+    demo = create_interface()
+    demo.launch()
